45dcf10edd1410a3d8c0afc20d23e0a37b22f577
aydncy <aydinceylan07@gmail.com>
2026-02-28 19:08:56 +0300

    fix: simplify EventStore

--- lib/services/event_store.dart
+++ lib/services/event_store.dart
@@ -1,4 +1,3 @@
-// lib/services/event_store.dart
 import '../models/event.dart';
 import '../data/demo_clinic.dart';
 
@@ -6,19 +5,15 @@ class EventStore {
   final List<Event> _events = [];
 
   EventStore() {
-    // Load demo seed events on startup
     _events.addAll(DemoClinic.seedEvents);
   }
 
-  /// All events - immutable (append-only)
   List<Event> get events => List.unmodifiable(_events);
 
-  /// Append a new event - never overwrites
   void append(Event event) {
     _events.add(event);
   }
 
-  /// Filter by aggregateId
   List<Event> eventsFor(String aggregateId) =>
       _events.where((e) => e.aggregateId == aggregateId).toList();
 }
