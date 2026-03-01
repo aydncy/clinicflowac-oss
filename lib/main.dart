a3d65852652373f7ab87cc10c10a554e20f8bc58
aydncy <aydinceylan07@gmail.com>
2026-03-01 17:40:26 +0300

    Fix Hive CE Flutter import path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
-
 import 'infrastructure/event_store/hive_event_store.dart';
 
 Future<void> main() async {
